c6fde1be63fbe6e364401d20db5ee52ab6297fb2
Mohammad Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2014-11-28 01:56:11 +0800

    Fixed query.

--- Ch 02/kickstart/insert.php
+++ Ch 02/kickstart/insert.php
@@ -2,7 +2,7 @@
 	$db = new PDO("mysql:host=localhost;dbname=kickstartapp", "USERNAME", "PASSWORD");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	try {
-  	$query = "INSERT INTO example(INSERTusername, password, email) VALUES('admin', MD5('admin'), 'youremail@domain.com' )";
+  	$query = "INSERT INTO example(username, password, email) VALUES('admin', MD5('admin'), 'youremail@domain.com' )";
 		$result = $db->query($query);
 	}catch (PDOException $e) {
     echo $e->getMessage();
